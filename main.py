2d904c1b4478303ed996b03d56ad0fee00204257
Eatowl <csnelcov@gmail.com>
2021-11-01 23:22:12 +0300

    start MNIST test work

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 import cv2
 import numpy as np
 import scipy.stats
-
+from keras.datasets import mnist
+from matplotlib import pyplot
 
 def viewImage(image, name_of_window):
 
@@ -158,4 +159,21 @@ def move_detect():
     cv2.destroyAllWindows()
 
 
-move_detect()
+#move_detect()
+
+
+# MNIST test neural networks
+
+#loading the dataset
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+#printing the shapes of the vectors
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
+
+for i in range(9):
+    pyplot.subplot(330 + 1 + i)
+    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
+pyplot.show()
