4faf7ca171c36d7cf65f02d34d36f2b0b83818c6
Eatowl <csnelcov@gmail.com>
2021-11-09 20:14:18 +0300

    [MNIST] Activation functions changet

--- main.py
+++ main.py
@@ -181,22 +181,24 @@ from matplotlib import pyplot
 #print(len(train_y))
 #pyplot.show()
 
-def relu(x):
+def tanh(x):
+    return np.tanh(x)
+
+def tanh2deriv(output):
+    return 1 - (output ** 2)
+
+def softmax(x):
+    temp = np.exp(x)
+    return temp / np.sum(temp, axis=1, keepdims=True)
+
+'''def relu(x):
     return (x > 0) * x
 
 def relu2deriv(output):
-    return output > 0
+    return output > 0'''
 
 def build_rand_weights(size_x, size_y):
     return 0.2 * np.random.random((size_x, size_y)) - 0.1
-
-def build_true(true_value):
-    true = np.zeros((1, 10))
-    for i in range(len(true[0])):
-        if i == true_value:
-            true[0][i] = 1.0
-
-    return true[0]
 
 def neural_network(inputs, weights):    
     return np.dot(inputs, weights)
@@ -216,55 +218,55 @@ for i, l in enumerate(test_y):
     test_labels[i][l] = 1
 
 
-alpha = 0.005
+alpha = 2
 hidden_size = 100
 pixel_per_image = 784
 num_labels = 10
 iterations = 300
+batch_size = 100
 
 np.random.seed(1)
 
-weights_0_1 = build_rand_weights(pixel_per_image, hidden_size)
-weights_1_2 = build_rand_weights(hidden_size, num_labels)
+weights_0_1 = 0.02 * np.random.random((pixel_per_image, hidden_size)) - 0.01
+weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
 
 for iter in range(iterations):
-    error, correct_cnt = (0.0, 0)
-    for i in range(len(images)):
+    correct_cnt = 0
+    for i in range(int(len(images) / batch_size)):
+        batch_start, batch_end = ((i * batch_size), ((i+1)*batch_size))
 
-        layer_0 = images[i:i+1]
-        layer_1 = relu(neural_network(layer_0, weights_0_1))
+        layer_0 = images[batch_start:batch_end]
+        layer_1 = tanh(neural_network(layer_0, weights_0_1))
         dropout_mask = np.random.randint(2, size=layer_1.shape)
         layer_1 *= dropout_mask * 2
+        layer_2 = softmax(neural_network(layer_1, weights_1_2))
+
+        for k in range(batch_size):
+            correct_cnt += int(np.argmax(layer_2[k:k+1]) ==\
+                               np.argmax(labels[batch_start+k:batch_start+k+1]))
+
+            layer_2_delta = (labels[batch_start:batch_end] - layer_2)\
+                                 / (batch_size * layer_2.shape[0])
+            layer_1_delta = layer_2_delta.dot(weights_1_2.T) * tanh2deriv(layer_1)
+            layer_1_delta *= dropout_mask
+
+            weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+            weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+    
+    test_correct_cnt = 0
+    for i in range(len(test_images)):
+        layer_0 = test_images[i:i+1]
+        layer_1 = tanh(neural_network(layer_0, weights_0_1))
         layer_2 = neural_network(layer_1, weights_1_2)
 
-        error += np.sum((labels[i:i+1] - layer_2) ** 2)
-        correct_cnt += int(np.argmax(layer_2) == np.argmax(labels[i:i+1]))
-
-        layer_2_delta = (labels[i:i+1] - layer_2)
-        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
-        layer_1_delta *= dropout_mask
-
-        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+        test_correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i+1]))
 
 
-    sys.stdout.write("\r"+" I:"+str(iter)+\
-                     " Error:"+str(error/float(len(images)))[0:5] +\
-                     " Correct:"+ str(correct_cnt/float(len(images))))
+    if iter % 10 == 0:
+        sys.stdout.write("\r"+" I:"+str(iter)+\
+                              " Correct:"+ str(correct_cnt/float(len(images)))+\
+                              " Test-Acc:" + str(test_correct_cnt/float(len(test_images))) + "\n")
 
-    error, correct_cnt = (0.0, 0)
-    if iter % 10 == 0 or iter == iterations - 1:
-
-        for i in range(len(test_images)):
-            layer_0 = test_images[i:i+1]
-            layer_1 = relu(neural_network(layer_0, weights_0_1))
-            layer_2 = neural_network(layer_1, weights_1_2)
-
-            error += np.sum((test_labels[i:i+1] - layer_2) ** 2)
-            correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i+1]))
-
-        sys.stdout.write(" Test-Err:" + str(error/float(len(test_images)))[0:5] +\
-                    " Test-Acc:" + str(correct_cnt/float(len(test_images))) + "\n")
 
 # example backpropagation
 
