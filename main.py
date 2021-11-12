c98b1375e0f42e87b6c6fdce1a3f3fd45967556c
Eatowl <csnelcov@gmail.com>
2021-11-12 18:59:39 +0300

    [MNIST] Convolutional neural network

--- main.py
+++ main.py
@@ -168,11 +168,17 @@ from matplotlib import pyplot
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
 alpha = 2
-hidden_size = 100
 pixel_per_image = 784
 num_labels = 10
 iterations = 300
-batch_size = 100
+batch_size = 128
+
+input_rows = 28
+input_cols = 28
+
+kernel_rows = 3
+kernel_cols = 3
+num_kernels = 16
 
 np.random.seed(1)
 
@@ -189,9 +195,13 @@ def softmax(x):
 def neural_network(inputs, weights):    
     return np.dot(inputs, weights)
 
+def get_image_section(layer, row_from, row_to, col_from, col_to):
+    section = layer[:, row_from:row_to, col_from:col_to]
+    return section.reshape(-1, 1, row_to - row_from, col_to - col_from)
 
 images, labels = (train_X[0:1000].reshape(1000, 28*28) / 255, train_y[0:1000])
 one_hot_labels = np.zeros((len(labels), 10))
+
 
 for i, l in enumerate(labels):
     one_hot_labels[i][l] = 1
@@ -202,7 +212,11 @@ test_labels = np.zeros((len(test_y), 10))
 for i, l in enumerate(test_y):
     test_labels[i][l] = 1
 
-weights_0_1 = 0.02 * np.random.random((pixel_per_image, hidden_size)) - 0.01
+
+hidden_size = ((input_rows - kernel_rows) * (input_cols - kernel_cols)) * num_kernels
+
+kernels = 0.02 * np.random.random((kernel_rows * kernel_cols, num_kernels)) - 0.01
+#weights_0_1 = 0.02 * np.random.random((pixel_per_image, hidden_size)) - 0.01
 weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
 
 for iter in range(iterations):
@@ -211,14 +225,30 @@ for iter in range(iterations):
         batch_start, batch_end = ((i * batch_size), ((i+1)*batch_size))
 
         layer_0 = images[batch_start:batch_end]
-        layer_1 = tanh(neural_network(layer_0, weights_0_1))
+        layer_0 = layer_0.reshape(layer_0.shape[0], 28, 28)
+        layer_0.shape
+
+        sects = []
+        for row_start in range(layer_0.shape[1] - kernel_rows):
+            for col_start in range(layer_0.shape[2] - kernel_cols):
+                sect = get_image_section(layer_0, row_start, row_start + kernel_rows,\
+                                                  col_start, col_start + kernel_cols)
+                sects.append(sect)
+
+        expanded_input = np.concatenate(sects, axis=1)
+        es = expanded_input.shape
+        flattened_input = expanded_input.reshape(es[0]*es[1], -1)
+
+        kernel_output = flattened_input.dot(kernels)
+        layer_1 = tanh(kernel_output.reshape(es[0],-1))
         dropout_mask = np.random.randint(2, size=layer_1.shape)
         layer_1 *= dropout_mask * 2
         layer_2 = softmax(neural_network(layer_1, weights_1_2))
 
         for k in range(batch_size):
-            correct_cnt += int(np.argmax(layer_2[k:k+1]) ==\
-                               np.argmax(labels[batch_start+k:batch_start+k+1]))
+            labelset = labels[batch_start+k:batch_start+k+1]
+            _inc = int(np.argmax(layer_2[k:k+1]) == np.argmax(labelset))
+            correct_cnt += _inc
 
         layer_2_delta = (labels[batch_start:batch_end] - layer_2)\
                                  / (batch_size * layer_2.shape[0])
@@ -226,12 +256,30 @@ for iter in range(iterations):
         layer_1_delta *= dropout_mask
 
         weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+        l1d_reshape = layer_1_delta.reshape(kernel_output.shape)
+        k_update = flattened_input.T.dot(l1d_reshape)
+        kernels -= alpha * k_update
     
     test_correct_cnt = 0
     for i in range(len(test_images)):
         layer_0 = test_images[i:i+1]
-        layer_1 = tanh(neural_network(layer_0, weights_0_1))
+        layer_0 = layer_0.reshape(layer_0.shape[0], 28, 28)
+        layer_0.shape
+
+        sects = []
+        for row_start in range(layer_0.shape[1] - kernel_rows):
+            for col_start in range(layer_0.shape[2] - kernel_cols):
+                sect = get_image_section(layer_0, row_start, row_start + kernel_rows,\
+                                                  col_start, col_start + kernel_cols)
+                sects.append(sect)
+
+        expanded_input = np.concatenate(sects, axis=1)
+        es = expanded_input.shape
+        flattened_input = expanded_input.reshape(es[0]*es[1], -1)
+
+
+        kernel_output = flattened_input.dot(kernels)
+        layer_1 = tanh(kernel_output.reshape(es[0],-1))
         layer_2 = neural_network(layer_1, weights_1_2)
 
         test_correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i+1]))
